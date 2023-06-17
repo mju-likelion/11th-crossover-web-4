e1d10a19545e7682ff660cf92b6f1fb0cae75315
zzzzzuuuuu <agu0830@naver.com>
2023-06-17 16:57:26 +0900

    style: 공통 버튼 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import GlobalStyle from './styles/GlobalStyles';
 import { ThemeProvider } from 'styled-components';
 import { Theme } from './styles/Theme.js';
+import NextButton from "./components/NextButton";
+import ContentButton from "./components/ContentButton";
 
 function App() {
   return (
     <ThemeProvider theme={Theme}>
-      <GlobalStyle>
-        <></>
-      </GlobalStyle>
+      <GlobalStyle />
+          <NextButton isFull="true">로그인</NextButton>
+      <ContentButton isWritten="true" isActive="false">작성하기</ContentButton>
     </ThemeProvider>
   );
 }
---
+++ src/components/NextButton.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from "styled-components";
+import {Theme} from "../styles/Theme"; //?
+
+const NextButton = ({children, isFull}) => {
+  return (
+    <>
+      <Button isfull={isFull}>{children}</Button>
+    </>
+  );
+};
+export default NextButton;
+
+const Button = styled.button`
+  width: 540px;
+  height: 90px;
+  border-radius: 25px;
+  color: white;
+  font-size: 36px;
+  line-height: 44px;
+  background-color: ${({isfull, theme}) => isfull==="true" ? theme.colors.BLUE2 : theme.colors.BLUE1};
+`
