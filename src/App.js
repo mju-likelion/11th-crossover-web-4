c5ba88070fc9076494f7e0774e67b64fb80d7fee
constantly-dev <rntqhdl10@naver.com>
2023-06-17 16:54:41 +0900

    signinput 미완성

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import './App.css';
 import GlobalStyle from './styles/GlobalStyles';
 import { ThemeProvider } from 'styled-components';
 import { Theme } from './styles/Theme.js';
-
+import SignInputBasic from './components/SignInputBasic';
 function App() {
   return (
     <ThemeProvider theme={Theme}>
-      <GlobalStyle>
-        <></>
-      </GlobalStyle>
+      <GlobalStyle />
+      <SignInputBasic></SignInputBasic>
     </ThemeProvider>
   );
 }
---
+++ src/components/SignInputBasic.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+// import { useState } from 'react';
+const SignInputBasic = () => {
+  // const [test, setTest] = useState('');
+
+  return (
+    <>
+      <SignInputContainer>
+        <SignInputWrapper>
+          <SignInputBox>
+            <SignInput></SignInput>
+            <SignIcon></SignIcon>
+          </SignInputBox>
+        </SignInputWrapper>
+        <HelperText>helper text</HelperText>
+      </SignInputContainer>
+    </>
+  );
+};
+const SignInputContainer = styled.div`
+  width: 540px;
+  height: 124px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const SignInputWrapper = styled.div`
+  width: 540px;
+  height: 90px;
+  border: 2px solid #000000;
+  background-color: #ffffff;
+  padding: 29px 28.5px;
+  margin-bottom: 10px;
+  border-radius: 25px;
+`;
+const SignInputBox = styled.div`
+  width: 483px;
+  height: 32px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+const SignInput = styled.input`
+  width: 440px;
+  height: 24px;
+  font-size: 20px;
+  border: none;
+`;
+const SignIcon = styled.div`
+  width: 32px;
+  height: 32px;
+`;
+const HelperText = styled.div`
+  width: 495px;
+  height: 19px;
+  color: rgba(0, 0, 0, 0.45);
+  font-size: 16px;
+  font-weight: 500;
+`;
+export default SignInputBasic;
