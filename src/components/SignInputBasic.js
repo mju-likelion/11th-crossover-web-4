edbf9837c40d9410b7b7d5e5fa38731cc871f143
constantly-dev <rntqhdl10@naver.com>
2023-06-20 18:54:26 +0900

    test: signinput 기능 동일 test 브랜치 추가

--- src/components/SignInputBasic.js
+++ src/components/SignInputBasic.js
@@ -1,21 +1,76 @@
 import styled from 'styled-components';
+import { validation } from './Validation';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import cancelIcon from '../assets/images/cancelicon.svg';
+import successIcon from '../assets/images/successicon.svg';
+import errorIcon from '../assets/images/erroricon.svg';
 
-const SignInputBasic = () => {
+const SignInputBasic = ({ name, placeholder, helperText }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    setValue,
+    // setValue는 register로 등록한 '값'을 변화시킬 수 있다.
+  } = useForm({
+    resolver: yupResolver(validation),
+    mode: 'onChange',
+  });
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  const inputValue = watch();
+
   return (
     <>
-      <AllContainer>
-        <BoxWrapper>
+      <AllContainer onSubmit={handleSubmit(onSubmit)}>
+        <BoxWrapper
+          iserror={errors[name] ? 'true' : 'false'}
+          issuccess={!errors[name] && inputValue[name] ? 'true' : 'false'}
+        >
           <InputBox>
-            <SignInput placeholder="기본값" />
-            <SignIcon />
+            <SignInput
+              type="text"
+              placeholder={placeholder}
+              name={name}
+              {...register(name)}
+            />
+            {inputValue[name] && !errors[name] ? (
+              <IsSuccessIcon src={successIcon} alt="successIcon" />
+            ) : errors[name] ? (
+              <IsSuccessIcon src={errorIcon} alt="errorIcon" />
+            ) : null}
+            {/* 값이 없으면 아이콘 null, 값이 생기고 에러 없으면 성공 / 유효성 에러면 에러 아이콘 생성 */}
+
+            {inputValue[name] ? (
+              <CancelIcon
+                src={cancelIcon}
+                alt="cancelIcon"
+                onClick={() => {
+                  setValue(name, '');
+                  errors[name] = '';
+                }}
+              ></CancelIcon>
+            ) : (
+              ''
+            )}
           </InputBox>
         </BoxWrapper>
-        <HelperText>helper text</HelperText>
+        {errors[name] ? (
+          <HelperText>{errors[name].message}</HelperText>
+        ) : inputValue[name] ? (
+          ''
+        ) : (
+          <HelperText>{helperText}</HelperText>
+        )}
       </AllContainer>
     </>
   );
 };
-const AllContainer = styled.div`
+const AllContainer = styled.form`
   width: 540px;
   height: 124px;
   display: flex;
@@ -25,7 +80,14 @@ const AllContainer = styled.div`
 const BoxWrapper = styled.div`
   width: 540px;
   height: 90px;
-  border: 2px solid ${(props) => props.theme.colors.GRAY};
+  border: 2px solid
+    ${({ iserror, theme, issuccess }) =>
+      iserror === 'true'
+        ? theme.colors.RED
+        : issuccess === 'true'
+        ? theme.colors.GREEN
+        : theme.colors.GRAY};
+
   background-color: #ffffff;
   padding: 29px 31px;
   margin-bottom: 10px;
@@ -45,11 +107,17 @@ const SignInput = styled.input`
   font-size: 20px;
   border: none;
 `;
-const SignIcon = styled.div`
+
+const IsSuccessIcon = styled.img`
   width: 32px;
   height: 32px;
 `;
-
+const CancelIcon = styled.img`
+  width: 32px;
+  height: 32px;
+  cursor: pointer;
+  margin-left: 8px;
+`;
 const HelperText = styled.div`
   width: 495px;
   height: 19px;
