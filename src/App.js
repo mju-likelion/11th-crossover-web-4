e21e2d93dedfd7310e245684d358544eddae76c6
zzzzzuuuuu <agu0830@naver.com>
2023-06-22 20:42:26 +0900

    feat: 라우터 연결

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import './App.css';
 import GlobalStyle from './styles/GlobalStyles';
-import { ThemeProvider } from 'styled-components';
-import { Theme } from './styles/Theme.js';
+import {ThemeProvider} from 'styled-components';
+import {Theme} from './styles/Theme.js';
 import NextButton from "./components/NextButton";
 import ContentButton from "./components/ContentButton";
 import Header from "./components/Header";
+import PostList from "./pages/Post/PostList";
+import {Route, Routes} from "react-router-dom";
+import WritePost from "./pages/WritePost/WritePost";
+import SignUp from "./pages/Join/SignUp";
+import Content from "./pages/Content/Content";
+
+function Login() {
+  return null;
+}
+
 function App() {
   return (
     <ThemeProvider theme={Theme}>
-      <GlobalStyle />
-      <Header />
-      <PostList/>
+      <GlobalStyle/>
+      <Routes>
+        {/*<Header/>*/}
+        <Route path="/" element={<PostList />}></Route>
+        <Route path="/:id" element={<Content />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/join" element={<SignUp />}></Route>
+        <Route path="/write" element={<WritePost />}></Route>
+      </Routes>
     </ThemeProvider>
   );
 }
