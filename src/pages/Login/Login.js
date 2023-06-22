22ef34e1a7c6bd0cd820e1e4af520cb1fca63a18
zzzzzuuuuu <agu0830@naver.com>
2023-06-22 20:00:12 +0900

    feat: 로그인 구현

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,23 +1,21 @@
 import React, {useEffect, useState} from 'react';
 import styled from "styled-components";
-import SignInputBasic from "../../components/SignInputBasic";
 import NextButton from "../../components/NextButton";
-import {useNavigate} from "react-router-dom";
 import {useForm} from "react-hook-form";
 import {yupResolver} from "@hookform/resolvers/yup";
 import {validation} from "../Join/Validation";
+import LoginInputBasic from "./LoginInputBasic";
+import Header from "../../components/Header";
 
 const Login = () => {
-  // const [checkLogin, setCheckLogin] = useState(false);
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: {errors},
     watch,
     setValue, // setValue는 register로 등록한 '값'을 변화시킬 수 있다.
   } = useForm({
     resolver: yupResolver(validation),
-    mode: 'onChange', // 실시간
   });
   const onSubmit = (data) => {
     console.log(data);
@@ -25,68 +23,68 @@ const Login = () => {
   const inputValue = watch();
 
   return (
+
     <>
-      <AllContainer>
-        <InputContainer>
-          <Title>로그인</Title>
-          <SignInputBasic
-            name="id"
-            placeholder="아이디"
-            helperText="영문과 숫자을 조합하여 5~10글자 미만으로 입력하여 주세요."
-            register={register}
-            handleSubmit={handleSubmit}
-            errors={errors}
-            setValue={setValue}
-            onSubmit={onSubmit}
-            inputValue={inputValue}
-          />
+      <Header isLogin="true" />
+        <AllContainer onSubmit={handleSubmit(onSubmit)}>
+          <InputContainer>
+            <Title>로그인</Title>
+            <LoginInputBasic
+              name="id"
+              placeholder="아이디"
+              helperText="영문과 숫자를 조합하여 5~10글자 미만으로 입력하여 주세요."
+              register={register}
+              errors={errors}
+              setValue={setValue}
+              inputValue={inputValue}
+            />
 
-          <SignInputBasic
-            name="password"
-            placeholder="비밀번호"
-            helperText="영문과 숫자, 특수기호를 조합하여 8~14 글자 미만으로 입력하여 주세요."
-            register={register}
-            handleSubmit={handleSubmit}
-            errors={errors}
-            setValue={setValue}
-            onSubmit={onSubmit}
-            inputValue={inputValue}
-          />
-        </InputContainer>
-        {inputValue.id && inputValue.password ? (
-          <NextButton children="로그인" isfull="true"></NextButton>
-        ) : (
-          <NextButton children="로그인"></NextButton>
-        )}
-      </AllContainer>
+            <LoginInputBasic
+              name="password"
+              placeholder="비밀번호"
+              helperText="영문과 숫자, 특수기호를 조합하여 8~14 글자 미만으로 입력하여 주세요."
+              register={register}
+              handleSubmit={handleSubmit}
+              errors={errors}
+              setValue={setValue}
+              onSubmit={onSubmit}
+              inputValue={inputValue}
+            />
+          </InputContainer>
+          {inputValue.id && inputValue.password ? (
+            <NextButton type="submit" children="로그인" isfull="true"></NextButton>
+          ) : (
+            <NextButton type="submit" children="로그인"></NextButton>
+          )}
+        </AllContainer>
 
-      {/*<LoginContainer>*/}
-      {/*  <Title>로그인</Title>*/}
-      {/*  <IDBox>*/}
-      {/*    <SignInputBasic*/}
-      {/*      placeholder='아이디'*/}
-      {/*      name='id'*/}
-      {/*      helperText='영문과 숫자를 조합하여 5~10글자 미만으로 입력하여 주세요.'*/}
-      {/*    />*/}
-      {/*  </IDBox>*/}
-      {/*  <PWBox>*/}
-      {/*    <SignInputBasic*/}
-      {/*      placeholder='비밀번호'*/}
-      {/*      name='password'*/}
-      {/*      helperText='영문과 숫자, 특수기호를 조합하여 8~14 글자 미만으로 입력하여 주세요.'*/}
-      {/*    />*/}
-      {/*  </PWBox>*/}
-      {/*  <NextButton*/}
-      {/*  >로그인</NextButton>*/}
-      {/*</LoginContainer>*/}
-      {/*<SignUp>회원가입</SignUp>*/}
-    </>
+        {/*<LoginContainer>*/}
+        {/*  <Title>로그인</Title>*/}
+        {/*  <IDBox>*/}
+        {/*    <SignInputBasic*/}
+        {/*      placeholder='아이디'*/}
+        {/*      name='id'*/}
+        {/*      helperText='영문과 숫자를 조합하여 5~10글자 미만으로 입력하여 주세요.'*/}
+        {/*    />*/}
+        {/*  </IDBox>*/}
+        {/*  <PWBox>*/}
+        {/*    <SignInputBasic*/}
+        {/*      placeholder='비밀번호'*/}
+        {/*      name='password'*/}
+        {/*      helperText='영문과 숫자, 특수기호를 조합하여 8~14 글자 미만으로 입력하여 주세요.'*/}
+        {/*    />*/}
+        {/*  </PWBox>*/}
+        {/*  <NextButton*/}
+        {/*  >로그인</NextButton>*/}
+        {/*</LoginContainer>*/}
+        {/*<SignUp>회원가입</SignUp>*/}
+      </>
   );
 };
 
 export default Login;
 
-const AllContainer = styled.div`
+const AllContainer = styled.form`
   width: 100%;
   height: 100%;
   display: flex;
