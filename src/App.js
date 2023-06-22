9314d6d07cdac0e8ce68f1e4858d9a100f1f40f5
zzzzzuuuuu <agu0830@naver.com>
2023-06-22 22:50:43 +0900

    style: 헤더

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import GlobalStyle from './styles/GlobalStyles';
-import { ThemeProvider } from 'styled-components';
-import { Theme } from './styles/Theme.js';
-import NextButton from './components/NextButton';
-import ContentButton from './components/ContentButton';
+import {ThemeProvider} from 'styled-components';
+import {Theme} from './styles/Theme.js';
 import Header from './components/Header';
 import PostList from './pages/Post/PostList';
-import { Route, Routes } from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import WritePost from './pages/WritePost/WritePost';
 import SignUp from './pages/Join/SignUp';
 import Content from './pages/Content/Content';
@@ -15,14 +13,14 @@ import Login from './pages/Login/Login';
 function App() {
   return (
     <ThemeProvider theme={Theme}>
-      <GlobalStyle />
+      <GlobalStyle/>
+      <Header/>
       <Routes>
-        {/*<Header/>*/}
-        <Route path="/" element={<PostList />}></Route>
-        <Route path="/:id" element={<Content />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/join" element={<SignUp />}></Route>
-        <Route path="/write" element={<WritePost />}></Route>
+        <Route path="/" element={<PostList/>}></Route>
+        <Route path="/:id" element={<Content/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/join" element={<SignUp/>}></Route>
+        <Route path="/write" element={<WritePost/>}></Route>
       </Routes>
     </ThemeProvider>
   );
